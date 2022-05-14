c7d26b2dd5b17b8c731bbb303312a75fb40ab25a
Shreshtha Misra <76113457+Shreshtha13@users.noreply.github.com>
2022-05-14 15:03:46 +0530

    Fix #7 - Add smooth scrolling to landing page (#55)
    * add blue filter to images on hover
    
    * fixed filter time delay
    
    * add blue filter on hover to alumni section
    
    * adjust position of social buttons on team page
    
    * remove comment
    
    * fixed alignment of Contact Us is footer
    
    * removed hover highlighting of icons on team images and restored highlighting in footer
    
    * remove comment
    
    * remove redundant css
    
    * Update README.md
    
    * added Sanidhya's info and image (#49)
    
    * added Aryan Gupta's info and image
    
    * Add existing file (#54)
    
    * added my image and info (#50)
    
    * added Tushar Sahu's image and info
    
    * Update team.yml
    
    * add blue filter to images on hover
    
    * remove redundant css
    
    * Fix #7 - Add smooth scrolling to scroll button on landing page
    
    Co-authored-by: Anirudh <anirudhdagar6@gmail.com>
    Co-authored-by: Sanidhya Singh <92792319+sanidhyas3s@users.noreply.github.com>
    Co-authored-by: Aryan Gupta <guptaaryan16@gmail.com>
    Co-authored-by: Devansh Bhardwaj <100514537+FireShadow05@users.noreply.github.com>
    Co-authored-by: Tushar Sahu <76245823+tushdon2@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
-jQuery(document).ready(function () {
-    "use strict";
-    new WOW().init();
+$(document).ready(function(){
+    $(".scroll-a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
 
-    (function () {
-        jQuery('.smooth-scroll').scrollingTo();
-    }());
 
-});
+
+
+
+
+
+
 
